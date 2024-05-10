e48fb96538c3a2ea407c9014148607376065fdc7
muskan <muskaann.129@gmail.com>
2024-05-10 16:52:00 +0530

    Feat: show all blogs & blog-detail

--- blog_app/blog_generator/views.py
+++ blog_app/blog_generator/views.py
@@ -11,6 +11,7 @@ import os
 import assemblyai as aai # type: ignore
 import openai # type: ignore
 from dotenv import load_dotenv
+from .models import BlogPost
 load_dotenv()
 
 # Create your views here.
@@ -72,12 +73,18 @@ def generate_blog(request):
     if not transcription:
       return JsonResponse({'error': " Failed to get transcript"}, status=500)
 
-
     # use OpenAI to generate the blog
     blog_content = generate_blog_from_transcription(transcription)
     if not blog_content:
       return JsonResponse({'error': " Failed to generate blog article"}, status=500)
 
+    new_blog = BlogPost.objects.create(
+      user=request.user,
+      youtube_title=title,
+      youtube_link=yt_link,
+      generated_content=blog_content
+    )
+    new_blog.save()
     return JsonResponse({'title': title, 'content': blog_content})
   else:
     return JsonResponse({'error': 'Invalid request method'}, status=405)
@@ -121,4 +128,15 @@ def generate_blog_from_transcription(transcription):
   return generated_blog
 
 
+def blog_list(request):
+  blog_articles = BlogPost.objects.filter(user=request.user)
+  return render(request, "all-blogs.html", {'blog_articles': blog_articles})
 
+
+def blog_detail(request, pk):
+  blog_article_detail = BlogPost.objects.get(id=pk)
+  if request.user == blog_article_detail.user:
+    return render(request, 'blog-details.html',{'blog_article_detail': blog_article_detail})
+  else:
+    return redirect('/')
+    
